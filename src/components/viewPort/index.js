1dbf3c27585e9f2cc8d4eae74ab95dca07b659db
asaran97 <asaran234@gmail.com>
2020-05-22 13:55:28 +0530

    added view port component

--- src/components/viewPort/index.js
+++ src/components/viewPort/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
-import './vie'
+import './style.scss'
+import { getHeightAndWidth } from '../../utils';
+import { useDispatch } from 'react-redux';
 
-const ViewPort = ({ view: { size, name } }) => {
+const ViewPort = ({
+  view: { size, name },
+  link,
+}) => {
+  const [width, height] = getHeightAndWidth(size);
+
   return (
     <div className='viewport'>
-      <iframe src="" frameborder="0" />
+      <iframe src={link} title={name} frameborder="0" height={height} width={width}/>
     </div>
   )
 }
--- src/components/views/index.js
+++ src/components/views/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './style.scss'
+import ViewPort from '../viewPort/index';
+import { useSelector } from 'react-redux';
+import { getSelected, getLink } from '../../actions/viewSelectors';
 
-const Views = () => (
-  <div className='views_container'>
-    <h1>content</h1>
-  </div>
-)
+const Views = () => {
+  const selectedViews = useSelector(getSelected);
+  const link = useSelector(getLink);
+
+  return (
+    <div className='views_container'>
+      {selectedViews.map(view => (<ViewPort view={view} link={link}/>))}
+    </div>
+  )
+}
 
 export default Views
