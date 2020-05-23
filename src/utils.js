3e6538f65746ad34b6519d88ade1ffa430e533b5
asaran97 <asaran234@gmail.com>
2020-05-23 14:01:40 +0530

    added dragable list

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,7 @@
 export const getHeightAndWidth = (size) => size.split('x')
+
+export const arrayMove = (array, from, to) => {
+  array = array.slice();
+  array.splice(to < 0 ? array.length + to : to, 0, array.splice(from, 1)[0]);
+  return array;
+};
