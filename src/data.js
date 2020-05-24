394d8832fde84795e5b1d2f99e12a8835d07f8f9
asaran97 <asaran234@gmail.com>
2020-05-24 20:22:59 +0530

    added device name to view data

--- src/data.js
+++ src/data.js
@@ -1,42 +1,50 @@
-export const mobileViews = [
+export const mobileViews = [ // id = 8
   {
     id: 1,
     size: '320x568',
-    name: 'iphone SE'
+    name: 'iphone SE',
+    device: 'mob'
   },
   {
     id: 2,
     size: '360x640',
-    name: 'moto G4'
+    name: 'moto G4',
+    device: 'mob'
   },
   {
     id: 3,
     size: '360x640',
-    name: 'galaxy S5'
+    name: 'galaxy S5',
+    device: 'mob'
   },
   {
     id: 4,
     size: '411x731',
-    name: 'pixel 2'
+    name: 'pixel 2',
+    device: 'mob'
   },
   {
     id: 5,
     size: '411x823',
-    name: 'pixel 2 xl'
+    name: 'pixel 2 xl',
+    device: 'mob'
   },
   {
     id: 6,
     size: '365x667',
-    name: 'iphone 6/7/8'
+    name: 'iphone 6/7/8',
+    device: 'mob'
   },
   {
     id: 7,
     size: '414x736',
-    name: 'iphone 6/7/8 +'
+    name: 'iphone 6/7/8 +',
+    device: 'mob'
   },
   {
     id: 8,
     size: '375x812',
-    name: 'iphone X'
+    name: 'iphone X',
+    device: 'mob'
   },
 ]
