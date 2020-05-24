901b8a8d0c3288df6bb2e8e3924784a9f5d38f05
asaran97 <asaran234@gmail.com>
2020-05-24 19:58:02 +0530

    saparated views data

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const mobileViews = [
+  {
+    id: 1,
+    size: '320x568',
+    name: 'iphone SE'
+  },
+  {
+    id: 2,
+    size: '360x640',
+    name: 'moto G4'
+  },
+  {
+    id: 3,
+    size: '360x640',
+    name: 'galaxy S5'
+  },
+  {
+    id: 4,
+    size: '411x731',
+    name: 'pixel 2'
+  },
+  {
+    id: 5,
+    size: '411x823',
+    name: 'pixel 2 xl'
+  },
+  {
+    id: 6,
+    size: '365x667',
+    name: 'iphone 6/7/8'
+  },
+  {
+    id: 7,
+    size: '414x736',
+    name: 'iphone 6/7/8 +'
+  },
+  {
+    id: 8,
+    size: '375x812',
+    name: 'iphone X'
+  },
+]
