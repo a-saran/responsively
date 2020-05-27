61ee88229b57dc0a5987df1fe783ec04fdea3440
asaran97 <asaran234@gmail.com>
2020-05-28 00:49:47 +0530

    created and added download modal to header

---
+++ src/components/download-apps/index.js
@@ -0,0 +1,35 @@
+import React, { useState, memo } from 'react';
+import { Modal } from 'react-responsive-modal';
+import { DownloadIcon } from '../icons';
+
+const MobileViewModal = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className='views-modal'>
+      <div className="view-btn" onClick={() => setOpen(true)}> <DownloadIcon /></div>
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+        classNames={{
+          overlay: 'app-modal-overlay',
+          modal: 'app-modal',
+          closeIcon: 'close-icon',
+        }}
+        center
+      >
+        <h2 className='modal-header'>
+          Download
+        </h2>
+        <div className="modal-body">
+          
+        </div>
+        <div className='modal-footer'>
+          
+        </div>
+      </Modal>
+    </div>
+  )
+}
+
+export default memo(MobileViewModal)
